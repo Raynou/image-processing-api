eece4d887fc77395b344883eb62b9ceb76803a1a
raynou <49658683+Raynou@users.noreply.github.com>
2025-11-07 01:30:22 -0600

    Add crypt secret to app config

--- src/auth/services/crypt.service.ts
+++ src/auth/services/crypt.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { createHmac } from 'node:crypto';
+import { cryptConstants } from '../constants';
 
 @Injectable()
 export class CryptService {
     hash(input: string): string {
-        return createHmac('sha256', 'secret')
+        const secret = cryptConstants.secret;
+        if (!secret) {
+            throw new Error("Crypt secret was not declared as environment variable")
+        }
+        return createHmac('sha256', secret)
             .update(input)
             .digest('hex')
     }
